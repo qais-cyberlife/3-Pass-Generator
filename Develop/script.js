bbb90183cca1d1be41ad672c14f4714b49a9a2e2
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-06-14 23:21:18 -0400

    it's ALIVE

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,80 @@ var Numbers = ['0','1','2','3','4','5','6','7','8','9']
 var LowercaseLetters = ['a','b','c','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var UppercaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var Symbols = ['`','~','!','@','#','$','%','^','&','*','(',')','_',',','{','}','[',']',':',';','.','<','>','/','?','\','|',']
-var all = Numbers.concat(LowercaseLetters.concat(UppercaseLetters.concat(Symbols)))
+
+
+// Prompt Code
+PassLength = prompt('Choose the Password Length.');
+IncludeNumbers = confirm('Do you Want to Include Numbers in your Password?');
+IncludeLwrcase = confirm('Do you Want to Include Lowercase Letters in your Password?');
+IncludeUpprCase = confirm('Do you Want to Include Uppercase Letters in your Password?');
+IncludeSymbols = confirm('Do you Want to Include Symbols in your Password?');
+
+console.log (PassLength)
+console.log (IncludeNumbers)
+console.log (IncludeLwrcase)
+console.log (IncludeUpprCase)
+console.log (IncludeSymbols)
+
+// NLUS
+if (IncludeNumbers == true & IncludeLwrcase == true & IncludeUpprCase == true & IncludeSymbols == true) {
+  var all = Numbers.concat(LowercaseLetters.concat(UppercaseLetters.concat(Symbols)))
+} 
+// LUS
+if (IncludeNumbers == false & IncludeLwrcase == true & IncludeUpprCase == true & IncludeSymbols == true ) {
+  var all = LowercaseLetters.concat(UppercaseLetters.concat(Symbols))
+} 
+// NUS
+if (IncludeNumbers == true & IncludeLwrcase == false & IncludeUpprCase == true & IncludeSymbols == true) {
+  var all = Numbers.concat(UppercaseLetters.concat(Symbols))
+} 
+// NLS
+if (IncludeNumbers == true & IncludeLwrcase == true & IncludeUpprCase == false & IncludeSymbols == true) {
+  var all = Numbers.concat(LowercaseLetters.concat(Symbols))
+} 
+// NLU
+if (IncludeNumbers == true & IncludeLwrcase == true & IncludeUpprCase == true & IncludeSymbols == false) {
+  var all = Numbers.concat(LowercaseLetters.concat(UppercaseLetters))
+} 
+// US
+if (IncludeNumbers == false & IncludeLwrcase == false & IncludeUpprCase == true & IncludeSymbols == true) {
+  var all = UppercaseLetters.concat(Symbols)
+} 
+// NS
+if (IncludeNumbers == true & IncludeLwrcase == false & IncludeUpprCase == false & IncludeSymbols == true) {
+  var all = Numbers.concat(Symbols)
+} 
+// NL
+if (IncludeNumbers == true & IncludeLwrcase == true & IncludeUpprCase == false & IncludeSymbols == false) {
+  var all = Numbers.concat(LowercaseLetters)
+} 
+// LS
+if (IncludeNumbers == false & IncludeLwrcase == true & IncludeUpprCase == false & IncludeSymbols == true) {
+  var all = LowercaseLetters.concat(Symbols)
+} 
+// NU
+if (IncludeNumbers == true & IncludeLwrcase == false & IncludeUpprCase == true & IncludeSymbols == false) {
+  var all = Numbers.concat(UppercaseLetters)
+} 
+// S
+if (IncludeNumbers == false & IncludeLwrcase == false & IncludeUpprCase == false & IncludeSymbols == true) {
+  var all = Symbols
+} 
+// N
+if (IncludeNumbers == true & IncludeLwrcase == false & IncludeUpprCase == false & IncludeSymbols == false) {
+  var all = Numbers
+} 
+// U
+if (IncludeNumbers == false & IncludeLwrcase == false & IncludeUpprCase == true & IncludeSymbols == false) {
+  var all = UppercaseLetters
+} 
+// L
+if (IncludeNumbers == false & IncludeLwrcase == false & IncludeUpprCase == false & IncludeSymbols == true) {
+  var all = LowercaseLetters
+} 
+
+console.log (all)
+console.log(all.length)
 
 // Defines the Generate Button as a Variable 
 var generateBtn = document.querySelector("#generate");
@@ -16,55 +89,12 @@ var generateBtn = document.querySelector("#generate");
 // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// These are empty Arrays corresponding to each character type they will contain the same elements
-//  as the original array but they will be shuffled
-// Ex. if Numbers was [1,2,3] ; numPass will be empty first and will be filled with perhaps [2,3,1] after line 30
-var numPass = [];
-var LwrPass = [];
-var UpprPass = [];
-var SymPass = [];
+// // These are empty Arrays corresponding to each character type they will contain the same elements
 var Pass = [];
 
-// Step 1 : write functions to all for loops
-// Step 2: make if statements depending on users input
-// Step 3: include all elements user wants based on input
-
-// Example: User input = 134
-// Include 1, 3, and 4 for loop through functions
-
-// This shuffles Array Numbers and pushes the randomized elements into the previously empty numPass; Line 36 logs Array numPass for 
-// experimintation purposes. 
-for (i= 0; i < Numbers.length; i ++){
-  var w = Math.floor(Math.random() * Numbers.length);
-  numPass.push(Numbers[w])
-}
-
-console.log (numPass)
-
-// This does the same but with Array LowercaseLetters
-for (i= 0; i < LowercaseLetters.length; i ++){
-  var x = Math.floor(Math.random() * LowercaseLetters.length);
-  LwrPass.push(LowercaseLetters[x])
-}
-
-console.log (LwrPass)
-
-// This does the same but with Array UppercaseLetters
-for (i= 0; i < UppercaseLetters.length; i ++){
-  var y = Math.floor(Math.random() * UppercaseLetters.length);
-  UpprPass.push(UppercaseLetters[y])
-}
-
-console.log (UpprPass)
-
-// This does the same but with Array Symbols
-for (i= 0; i < Symbols.length; i ++){
-  var z = Math.floor(Math.random() * Symbols.length);
-  SymPass.push(Symbols[z])
-}
 
 // This picks a randomizes the combined Array and joins the elements (eliminates the commas) and logs it in 
-for (i= 0; i < all.length; i ++){
+for (i= 0; i < PassLength; i ++){
   var a = Math.floor(Math.random() * all.length);
   char = all[a];
   Pass.push(char);
@@ -78,5 +108,6 @@ function writePassword() {
   var password = PassNC;
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 }
-
